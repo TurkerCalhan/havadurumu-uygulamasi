858f33ddeedaaa5ce43afacece3af3c50e327c9b
TurkerCalhan <turker.calhan@outlook.com>
2022-06-27 18:01:06 +0300

    React-Hava-Durumu-App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import "./index.css"
+
+import ArkaPlan from "./Bileşenler/Arkaplan";
+import Arama from "./Bileşenler/Arama/AramaCubugu";
+import Sonuc from "./Bileşenler/Sonuc"
 
 function App() {
+    
+  const [havaDurumu, belirtHavaDurumu] = useState({});
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="uygulama">
+      <ArkaPlan arkaplan={havaDurumu.weather && havaDurumu.weather[0].main} />
+      <div id="baslik">Hava Durumu</div>
+      <Arama belirtHavaDurumu={belirtHavaDurumu} />
+      <Sonuc hava={havaDurumu}/>
+    </section>
   );
 }
 
